4b32a392d7d6ae4aa81f89e44e8cacf60dabbed8
Andrey Yasko <andrey@yasko.org>
2026-08-18 07:25:20 -0700

    Clean up install remove-folder copy
    Fix broken last-step English and add a matching title. una-g only.

--- install/templates/remove_install.php
+++ install/templates/remove_install.php
@@ -1,5 +1,7 @@
 <div class="bx-install-remove-install">
 
+    <h1 class="bx-def-font-h1"><?php echo _t('_sys_inst_remove_install'); ?></h1>
+
     <p class="bx-install-step-lead"><?php echo _t('_sys_inst_msg_remove_install'); ?></p>
 
 </div>
